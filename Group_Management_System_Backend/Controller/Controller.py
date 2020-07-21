2b0c8a43e1443948dd0be12bd305d1e8be947124
Harsh Patel <hr402607@dal.ca>
2020-07-21 17:37:53 -0300

    login part added in backend

--- Group_Management_System_Backend/Controller/Controller.py
+++ Group_Management_System_Backend/Controller/Controller.py
@@ -1,15 +1,21 @@
-
-from flask import Flask, Blueprint, render_template,request
+from flask import Flask, Blueprint, render_template, request
 from Service import User_Management
+
 app = Flask(__name__)
 
-controller = Blueprint('controller',__name__,template_folder='templates')
+controller = Blueprint('controller', __name__, template_folder='templates')
+
 
 @controller.route('/')
 def hello_world():
     return render_template("index.html")
 
-@controller.route('/register', methods=['POST'])
+@controller.route('/register', methods=["GET"])
+def register_page():
+    print("here")
+    return render_template("register.html")
+
+@controller.route('/register', methods=["POST"])
 def register():
     first_name = request.form["firstname"]
     last_name = request.form["lastname"]
@@ -18,5 +24,16 @@ def register():
     confirm_password = request.form["confirm_password"]
     security_1 = request.form["security_1"]
     security_2 = request.form["security_2"]
-    User_Management.register_service(first_name,last_name,email,password,confirm_password,security_1,security_2)
-    return render_template("index.html")
+    flag, message = User_Management.register_service(first_name, last_name, email, password, confirm_password,
+                                                     security_1, security_2)
+
+    return render_template("register.html", message=message)
+
+
+@controller.route("/login", methods=["POST"])
+def login():
+    email = request.form["email"]
+    password = request.form["password"]
+    security_question = request.form["security_question"]
+    flag, message = User_Management.login_service(email, password, security_question)
+    return render_template("login.html")
--- Group_Management_System_Backend/Service/User_Management.py
+++ Group_Management_System_Backend/Service/User_Management.py
@@ -1,4 +1,21 @@
-
-def register_service(first_name,last_name,email,password,confirm_password,security_1,security_2):
+def register_service(first_name, last_name, email, password, confirm_password, security_1, security_2):
     print("registration for user here")
     # Lambda function will encrypt and decrypt the password and send it to storage.
+
+    flag = True
+    if(flag==True):
+        message = "User registered successfully"
+    else:
+        message = "User did not register"
+    return True, message
+
+
+def login_service(email, password, security_question):
+    print("login service for user here")
+
+    flag = True
+    if(flag == True):
+        message = "Login successfull"
+    else:
+        message = "Login unsuccessful"
+    return True,message
