03b8d42dca9b2ac3285acfdb90cf10204e64d93e
Harsh Patel <hr402607@dal.ca>
2020-07-21 17:14:50 -0300

    backend for registration module created

--- Group_Management_System_Backend/Controller/Controller.py
+++ Group_Management_System_Backend/Controller/Controller.py
@@ -1,6 +1,6 @@
 
 from flask import Flask, Blueprint, render_template,request
-
+from Service import User_Management
 app = Flask(__name__)
 
 controller = Blueprint('controller',__name__,template_folder='templates')
@@ -11,6 +11,12 @@ def hello_world():
 
 @controller.route('/register', methods=['POST'])
 def register():
-    # form = request.form["register_form"]
-    # print(form)
+    first_name = request.form["firstname"]
+    last_name = request.form["lastname"]
+    email = request.form["email"]
+    password = request.form["password"]
+    confirm_password = request.form["confirm_password"]
+    security_1 = request.form["security_1"]
+    security_2 = request.form["security_2"]
+    User_Management.register_service(first_name,last_name,email,password,confirm_password,security_1,security_2)
     return render_template("index.html")
---
+++ Group_Management_System_Backend/Service/User_Management.py
@@ -0,0 +1,4 @@
+
+def register_service(first_name,last_name,email,password,confirm_password,security_1,security_2):
+    print("registration for user here")
+    # Lambda function will encrypt and decrypt the password and send it to storage.
